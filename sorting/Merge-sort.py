eed24d669f782e71a4158df58f21e59d616458fc
Gouravsingh21 <gaurav.singh21062000@gmail.com>
2020-07-14 01:18:16 +0530

    Merge sort

---
+++ sorting/Merge-sort.py
@@ -0,0 +1,31 @@
+# Python program to implement merge sort
+def divide(lst):
+        if len(lst)>1:
+            mid=len(lst)//2
+            l=lst[:mid]
+            r=lst[mid:]
+            divide(l)
+            divide(r)
+            i=0
+            j=0
+            k=0
+            while i<len(l) and j <len(r):
+                if l[i]<r[j]:
+                    lst[k]=l[i]
+                    i=i+1
+                    k=k+1
+                else:
+                    lst[k]=r[j]
+                    j=j+1
+                    k=k+1
+            while i<len(l):
+                lst[k]=l[i]
+                i=i+1
+                k=k+1
+            while j<len(r):
+                lst[k]=r[j]
+                j=j+1
+                k=k+1
+lst=[3,1,0,4,2]
+divide(lst)
+print('sorted list',lst)
