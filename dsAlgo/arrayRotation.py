8c970aff584e83cabe01a5b5690bc442df57620f
Gouravsingh21 <gaurav.singh21062000@gmail.com>
2020-07-27 02:33:16 +0530

    algorithm for array rotation

---
+++ dsAlgo/arrayRotation.py
@@ -0,0 +1,26 @@
+#write a python program to rotate a array
+import array as arr
+def rotate(ar,n,ro):
+    for i in range(0,n):
+        first=ar[0]
+        if i<ro:
+            for j in range(1,n+1):
+                ar[j-1]=ar[j]
+                if j==n-1:
+                    ar[n-1]=first
+                    break
+        else:
+            break
+    display(ar,n)
+def display(ar,n):
+    print('Rotated array',end=' ')
+    for i in range(0,n):
+        print(ar[i],end=' ')
+    print('\r')
+def main():
+    a=arr.array('i',[1,2,3,4,5,6,7])
+    n=len(a)
+    #r=int(input('enter the no. of elements you wnat to rotate'))
+    r=2
+    rotate(a,n,r)
+main()
